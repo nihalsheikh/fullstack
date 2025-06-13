1d94ed9a323f06e5e96df49767d633dcbf909c9c
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-13 10:26:29 +0530

    JS Part 9 Completed

---
+++ 04. JavaScript/Part 09/dom.js
@@ -0,0 +1,10 @@
+let posters = document.getElementsByClassName("oldImg");
+
+for(let i=0; i<posters.length; i++) {
+    console.dir(posters[i]);
+}
+
+let para = document.querySelector("p");
+para.innerText;
+para.innerHTML;
+para.textContent;
