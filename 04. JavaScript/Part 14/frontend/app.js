ef4c6efe62a364541a48691ac7e08e90efa7770d
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-16 16:53:34 +0530

    Accessing Prototype

--- 04. JavaScript/Part 14/frontend/app.js
+++ 04. JavaScript/Part 14/frontend/app.js
@@ -1,1 +1,19 @@
 // OOPS with JavaScript
+// Prototype: built-in property in every object
+// ex: Arrays in JS are internally considered as Objects
+let arr = [1,2,3,4]
+console.log(arr);
+
+// you can see the output in browser console as >(4)[1,2,3,4]
+// and when you expand it you can see something like the following:
+// 0: 1
+// 1: 2
+// 2: 3
+// 3: 4
+// [[Prototype]]: Array(0)
+// All the functions that are applied to array are basically stored in Prototype, and Array inherits them
+
+// lets make a func for only arr
+// This func is unique to arr, if we create another arr2,
+// it will not have this func as it is not the part of prototype
+arr.sayHello = () => console.log("This is arr");
