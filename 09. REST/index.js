59b3bbf37f496f8adb95cbe3c92a03ca0003d335
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-17 16:37:13 +0530

    started the server

--- 09. REST/index.js
+++ 09. REST/index.js
@@ -1,1 +1,33 @@
 // Quora Page: Understanding RESTful API
+// require Express
+const express = require("express");
+const app = express();
+
+// define port
+const port = 8080;
+
+// define path for EJS
+const path = require("path");
+
+// define middleware
+app.use(express.urlencoded({ extended: true }))
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set path for views folder
+app.set("views", path.join(__dirname, "views"));
+
+// set path for public folder
+app.set(express.static(path.join(__dirname, "public")));
+
+// make sure port is running and active
+app.listen(port, (req, res) => {
+    console.log(`listening on Port ${port}`)
+})
+
+
+// very basic GET Request
+app.get("/", (req, res) => {
+    res.send("Root Page, server is running...")
+})
