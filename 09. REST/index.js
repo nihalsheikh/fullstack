08ad5884c48e8cfbab7403b65659442ae4368998
Nihal Sheikh <nihalsheikh585@gmail.com>
2025-06-17 16:50:46 +0530

    creating GET Requests

--- 09. REST/index.js
+++ 09. REST/index.js
@@ -10,7 +10,7 @@ const port = 8080;
 const path = require("path");
 
 // define middleware
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
@@ -24,10 +24,37 @@ app.set(express.static(path.join(__dirname, "public")));
 // make sure port is running and active
 app.listen(port, (req, res) => {
     console.log(`listening on Port ${port}`)
-})
+});
 
+// We are not using and database, so we are making our own data, using arrays
+// we want our DB to be able to handle changes, so we use 'let' and not 'const'
+let posts = [
+    {
+        username: "johnwick",
+        content: "am I still ex-communicado"
+    },
+    {
+        username: "joemama",
+        content: "the joke never expires"
+    },
+    {
+        username: "some.developer",
+        content: "man, when will i get a job???"
+    },
+    {
+        username: "recruiter",
+        content: "we gonna farm view and reach and not give jobs"
+    },
+];
+
+// new GET Request to set-up our posts
+// resource: things/objects on which we perform out CRUD operations
+// make sure the name of the route you set is a 'noun' and not a 'verb' ex: posts and not viewPost or seepost
+app.get("/posts", (req, res) => {
+    res.send("Posts are uploading... Please hold on a little longer")
+})
 
 // very basic GET Request
 app.get("/", (req, res) => {
     res.send("Root Page, server is running...")
-})
+});
