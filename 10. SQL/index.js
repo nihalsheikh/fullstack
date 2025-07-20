d019299b19e1a46ac25939f3b358bd2ceaccb74d
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-20 17:58:16 +0530

    Inserting values in Bulk using Faker

--- 10. SQL/index.js
+++ 10. SQL/index.js
@@ -6,16 +6,6 @@ const { faker } = require('@faker-js/faker');
 // connecting node with db
 const mysql = require('mysql2');
 
-// generating random data
-let getRandomUser = () => { // create: id, name, email, password
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-}
-
 // check if the data is generating properly
 // console.log(getRandomUser())
 
@@ -27,17 +17,31 @@ const connection = mysql.createConnection({
   password: 'mysql@123'
 });
 
+// generating random data
+// When inserting data in bulk, return an Array instead of an object
+let getRandomUser = () => { // create: id, name, email, password
+  return [
+    faker.string.uuid(),
+    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+}
+
 // use connection methods to check or making chnages to DB
 // Exception Handling: write  in try catch block, we never know what may go wrong
 // we can write query else where and then pass it to our query connection method
+// this is the SQL QUERY for inserting multiple values
 let query = "INSERT INTO user (id, username, email, password) VALUES ?";
-let user1 = ["123","123_new_user", "xyz@yahoo.com", "abc"];
-let users = [
-    ["123b","123_new_userb", "bxyz@yahoo.com", "abcb"],
-    ["123c","123_new_userc", "cxyz@yahoo.com", "abcc"]
-]
+
+let data = [];
+for(let i=1; i<=100; i++) {
+    // here we get 100 random fake user data in arr form
+    data.push(getRandomUser());
+}
+
 try {
-    connection.query(query, [users], (err, result) => {
+    connection.query(query, [data], (err, result) => {
         if (err) throw err;
         console.log(result);
         console.log(result.length);
