991b0b645c71a433e4cc6ec90f35016effbad60f
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-20 16:33:59 +0530

    Establishing Accessing DB connection

--- 10. SQL/index.js
+++ 10. SQL/index.js
@@ -3,6 +3,9 @@
 // creating random data from faker
 const { faker } = require('@faker-js/faker');
 
+// connecting node with db
+const mysql = require('mysql2');
+
 // generating random data
 let getRandomUser = () => { // create: id, name, email, password
   return {
@@ -14,4 +17,23 @@ let getRandomUser = () => { // create: id, name, email, password
 }
 
 // check if the data is generating properly
-console.log(getRandomUser())
+// console.log(getRandomUser())
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'newuser',
+  database: 'delta',
+  password: 'mysql@123'
+});
+
+// use connection methods to check or making chnages to DB
+// Exception Handling: write  in try catch block, we never know what may go wrong
+try {
+    connection.query("SHOW TABLES", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    })
+} catch (err) {
+    console.log(err);
+}
