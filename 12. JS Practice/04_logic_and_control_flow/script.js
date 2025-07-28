0cfa733e624950e010c8493456bb9521ca57c340
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-28 21:26:28 +0530

    Logic and Control Flow

---
+++ 12. JS Practice/04_logic_and_control_flow/script.js
@@ -0,0 +1,23 @@
+console.log("Logic and Control Flow")
+
+/*
+Conditional Statements: if, else if, else
+ - syntax:
+            if (<c1>) {
+                // some code
+            } else if (<c2>) {
+                // some code again
+            } else {
+                // remaining code
+            }
+*/
+
+const age = 18;
+
+if (age > 18) {
+    console.log("You can enter")
+} else if (age === 18) {
+    console.log("You just turned 18, welcome!")
+} else {
+    console.log("Grow up!")
+}
