a3e2302a814ff0dccbf07b7bafbb8fbe369a1cc0
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-30 18:32:41 +0530

    Truthy and Falsy values

--- 12. JS Practice/04_logic_and_control_flow/script.js
+++ 12. JS Practice/04_logic_and_control_flow/script.js
@@ -12,12 +12,19 @@ Conditional Statements: if, else if, else
             }
 */
 
-const age = 18;
+// const age = 18;
 
-if (age > 18) {
-    console.log("You can enter")
-} else if (age === 18) {
-    console.log("You just turned 18, welcome!")
+// if (age > 18) {
+//     console.log("You can enter")
+// } else if (age === 18) {
+//     console.log("You just turned 18, welcome!")
+// } else {
+//     console.log("Grow up!")
+// }
+
+// Falsy Values:    false, 0, '', null, undefined, NaN
+if (0) {
+    console.log("Truthy")
 } else {
-    console.log("Grow up!")
+    console.log("Falsy")
 }
