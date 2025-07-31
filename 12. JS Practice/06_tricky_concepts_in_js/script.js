70ef72f62f1f1caca1b14914e632d02968249357
nihalsheikh <nihalsheikh585@gmail.com>
2025-07-31 15:23:30 +0530

    Scope, Hoisting and Closure

--- 12. JS Practice/06_tricky_concepts_in_js/script.js
+++ 12. JS Practice/06_tricky_concepts_in_js/script.js
@@ -1,1 +1,22 @@
-console.log("******** Tricky Concepts in JS ********")
+console.log("******* Tricky Concepts in JS *******")
+
+// Global Scope
+console.log("\n######## Global Scope #########")
+const fname = "John";
+
+const logName = () => {
+    console.log(fname);
+}
+
+logName();
+
+// Local Scope or Function Scope
+console.log("\n######## Function Scope #########")
+const someFunc = () => {
+    // This is the local scope, whcih is inside "{}"
+    const age = 20;
+
+    console.log(age);
+}
+// console.log(age) // cannot access the age variable outside the func/local scope
+someFunc();
