8e6efbf2db0498a6a0347bef544f1e0d836c42a5
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-05 13:26:11 +0530

    Value vs Reference module complete

---
+++ 12. JS Practice/10_value_vs_reference/script.js
@@ -0,0 +1,8 @@
+console.log("****** Value vs Reference ******")
+
+// primitive : value
+// complex: reference
+// spread operator (...)
+// Shallow cloning
+    // cloning arrays
+    // cloning objects
