8785f77cdb2aa0fed591aa2f715651d922e1cb4f
nihalsheikh <nihalsheikh585@gmail.com>
2025-08-06 18:55:17 +0530

    class, new, this, functional apprach to class

--- 12. JS Practice/12_classes_new_this/script.js
+++ 12. JS Practice/12_classes_new_this/script.js
@@ -1,2 +1,48 @@
 console.log("****** classes, new and this ******")
- 
+
+// this keyword
+console.log("\n this")
+const person = {
+    name: "John",
+    getName() {
+        console.log(this)
+    }
+}
+
+person.getName()
+
+// class
+console.log("\n Class")
+class Person {
+    constructor(name, age, isWorking) {
+        this.name = name;
+        this.age = age;
+        this.isWorking = isWorking
+    }
+}
+
+const user = new Person("John", 24, false)
+console.log(user)
+
+
+// Functional Approach to JS
+console.log("\nFunctional Approach to class")
+// const createPerson = (name, age, isWorking) => {
+//     const userSchema = {
+//         name: name,
+//         age: age,
+//         isWorking: isWorking
+//     }
+//     return userSchema
+// }
+// const createPerson = ( name, age, isWorking ) => {
+//     const userSchema = { name, age, isWorking }
+//     return userSchema
+// }
+// const createPerson = ( name, age, isWorking ) => {
+//     return { name, age, isWorking }
+// }
+const createPerson = ( name, age, isWorking ) =>  ({ name, age, isWorking });
+
+const user1 = createPerson("Joe", 24, true)
+console.log(user1)
